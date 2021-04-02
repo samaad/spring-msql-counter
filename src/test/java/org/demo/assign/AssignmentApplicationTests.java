429d1e88ef37926d8f00503eccea3503c165a193
shoaib <shoaib.doulatabad@gmail.com>
2021-04-02 15:51:21 +0400

    updated the test file

--- src/test/java/org/demo/assign/AssignmentApplicationTests.java
+++ src/test/java/org/demo/assign/AssignmentApplicationTests.java
@@ -3,11 +3,11 @@ package org.demo.assign;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AssignmentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
